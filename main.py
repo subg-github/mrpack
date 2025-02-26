b0f4b7488a15511128b4c6a91e7d7fe52302c62b
Zephrynis <132010650+subg-github@users.noreply.github.com>
2025-02-26 22:28:53 +0000

    Feat: list all mods

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
+import os
+
+dir = os.getcwd()
+
+print(os.listdir(f"{dir}//mods"))
+
 with open('pack.mrpack', 'w') as file:
-    file.write("Hello World!")
+    file.write(os.listdir(f"{dir}//mods"))
