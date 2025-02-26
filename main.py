d60e3d12fe098d1df5d03156be81cfb550d4b1f6
Zephrynis <132010650+subg-github@users.noreply.github.com>
2025-02-26 22:44:00 +0000

    Fix: wrong gh domain in urls

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ repo = sys.argv[1]
 branch = sys.argv[2]
 
 for i in os.listdir(f"{dir}//mods"):
-    print(f"https://github.com/{repo}/refs/heads/{branch}/mods/{i}")
+    print(f"https://raw.githubusercontent.com/{repo}/refs/heads/{branch}/mods/{i}")
 
 with open('pack.mrpack', 'w') as file:
     for i in os.listdir(f"{dir}//mods"):
-        file.write(f"https://github.com/{repo}/refs/heads/{branch}/mods/{i}")
+        file.write(f"https://raw.githubusercontent.com/{repo}/refs/heads/{branch}/mods/{i}")
