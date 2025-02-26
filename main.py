3725b17c195b7be7cc2ac73fc3aa080dac2135e8
Zephrynis <132010650+subg-github@users.noreply.github.com>
2025-02-26 23:15:35 +0000

    Feat: json output & pack export to zip

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import os
 import sys
+import json
+import hashlib
+from zipfile import ZipFile
 
 dir = os.getcwd()
 
@@ -9,6 +12,48 @@ branch = sys.argv[2]
 for i in os.listdir(f"{dir}//mods"):
     print(f"https://raw.githubusercontent.com/{repo}/refs/heads/{branch}/mods/{i}")
 
-with open('pack.mrpack', 'w') as file:
+with open('modrinth.index.json', 'w') as file:
+    json = {
+        "formatVersion": "1",
+        "game": "minecraft",
+        "versionId": branch,
+        "name": "Test mrpack",
+        "files": [],
+        "dependencies": {
+            "minecraft": "1.20.1",
+            "forge": "47.3.0"
+        }
+    }
     for i in os.listdir(f"{dir}//mods"):
-        file.write(f"https://raw.githubusercontent.com/{repo}/refs/heads/{branch}/mods/{i}")
+
+        hash_func = hashlib.new("sha1")
+        with open(f"mods/{i}", 'rb') as file:
+            while chunk := file.read(8192):  # Read the file in chunks of 8192 bytes
+                hash_func.update(chunk)
+        sha1 = hash_func.hexdigest()
+
+        hash_func = hashlib.new("sha512")
+        with open(f"mods/{i}", 'rb') as file:
+            while chunk := file.read(8192):  # Read the file in chunks of 8192 bytes
+                hash_func.update(chunk)
+        sha512 = ""
+
+        filejson = {
+            "path": f"mods/{i}",
+            "hashes": {
+                "sha1": sha1,
+                "sha512": sha512
+            },
+            "downloads": [
+                f"https://raw.githubusercontent.com/{repo}/refs/heads/{branch}/mods/{i}"
+            ],
+            "fileSize": os.path.getsize(f"{dir}//mods//{i}")
+        }
+
+        json["files"].append(filejson)
+
+    output = json.dumps(dictionary, indent=4)
+    file.write(output)
+
+with ZipFile('pack.mrpack', 'w', zipfile.ZIP_DEFLATED) as pack:
+    pack.write("modrinth.index.json")
