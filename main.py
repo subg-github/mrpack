12efc576d41e2920beae4aeb2c242f9a2c7c17ec
Zephrynis <132010650+subg-github@users.noreply.github.com>
2025-02-26 22:42:33 +0000

    Fix: incorrect file path

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ import sys
 dir = os.getcwd()
 
 repo = sys.argv[1]
+branch = sys.argv[2]
 
 for i in os.listdir(f"{dir}//mods"):
-    print(f"https://github.com/{repo}/mods/{i}")
+    print(f"https://github.com/{repo}/refs/heads/{branch}/mods/{i}")
 
 with open('pack.mrpack', 'w') as file:
     for i in os.listdir(f"{dir}//mods"):
-        file.write(f"https://github.com/{repo}/mods/{i}")
+        file.write(f"https://github.com/{repo}/refs/heads/{branch}/mods/{i}")
